58e67a101523a3c08de52ba364dc5ca26aac89cf
minhquan1313 <Minhquan1313@gmail.com>
2021-05-07 20:43:58 +0700

    a

--- libs/app.js
+++ libs/app.js
@@ -1,6 +1,12 @@
-document.getElementById("bgAudio").volume = 0.1;
-var optionList = document.querySelectorAll("li.optionList__item");
+function urlNameToNormalName(e) {
+  console.log(decodeURI(e));
+  return decodeURI(e);
+}
 
+document.getElementById("bgAudio").volume = 0.1;
+
+//Thay doi khi re chuot cac Options
+var optionList = document.querySelectorAll("li.optionList__item");
 for (var i of optionList) {
   i.addEventListener("mouseover", function () {
     this.classList.add("optionList__item--selected");
@@ -10,23 +16,44 @@ for (var i of optionList) {
   });
 }
 
+//Lay audio
 var audioControl = document.getElementById("audioControl");
+//Lay icon player cua audio
 var bgMusic = document.getElementById("bgAudio");
 
+//Lay ten bai hat
+var musicName = [bgMusic.src].toString().split("/").pop();
+musicName = musicName.slice(0, musicName.lastIndexOf("."));
+// console.log(musicName);
+musicName = urlNameToNormalName(musicName);
+
+var musicPaused = document.querySelector('i[class*="ti-control-play"]');
+var musicPlaying = document.querySelector('i[class*="ti-control-pause"]');
+
+// <==> on("load",function(){})
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
-    console.log("fully loaded");
-    // document ready
-    if (bgMusic.duration > 0 && !bgMusic.paused) {
-      audioControl.innerHTML = `<i class="ti-control-pause"></i>`;
-    }
+    var nameAudio = audioControl.querySelector(".audioControl__Name");
+    nameAudio.innerHTML = musicName;
+
+    var d = setInterval(function () {
+      if (!bgMusic.paused) {
+        musicPlaying.setAttribute("style", "display: block");
+        musicPaused.setAttribute("style", "display: none");
+        bgMusic.play();
+        clearInterval(d);
+      }
+    }, 200);
 
     audioControl.onclick = function () {
-      if (audioControl.firstElementChild.className.includes("pause")) {
-        audioControl.innerHTML = `<i class="ti-control-play"></i>`;
+      // bgMusic.duration > 0 &&
+      if (!isNaN(bgMusic.duration) && bgMusic.duration > 0 && !bgMusic.paused) {
+        musicPlaying.setAttribute("style", "display: none");
+        musicPaused.setAttribute("style", "display: block");
         bgMusic.pause();
-      } else {
-        audioControl.innerHTML = `<i class="ti-control-pause"></i>`;
+      } else if (!isNaN(bgMusic.duration)) {
+        musicPlaying.setAttribute("style", "display: block");
+        musicPaused.setAttribute("style", "display: none");
         bgMusic.play();
       }
     };
