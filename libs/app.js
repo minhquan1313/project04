9083668990622bf30cd48f6c444c4128b86fe91c
minhquan1313 <Minhquan1313@gmail.com>
2021-05-08 20:53:50 +0700

    a

--- libs/app.js
+++ libs/app.js
@@ -100,3 +100,92 @@ for (var i of albumPics) {
     let imgLink = e.target.src;
   });
 }
+
+//PopUp
+const popup = document.getElementById("popup");
+const delay = 200;
+
+function getPopup() {
+  popup.setAttribute(
+    "style",
+    `display: flex; transition: all ${delay / 1000}s ease; opacity: 0;`
+  );
+  setTimeout(function () {
+    popup.style.opacity = 1;
+  }, 0);
+}
+
+function shutPopup() {
+  popup.setAttribute("style", "display: none");
+}
+
+popup.onclick = function (e) {
+  if (e.target.className == popup.className) {
+    // console.log("Clicked popup background only");
+
+    popup.setAttribute(
+      "style",
+      `transition: all ${delay / 1000}s ease; pointer-events: none; opacity: 0;`
+    );
+
+    setTimeout(function () {
+      popup.setAttribute("style", "display: none;");
+      popup.innerHTML = "";
+    }, delay);
+  }
+};
+
+//Error
+function popupError(e) {
+  // console.log(e);
+  getPopup();
+  const popupError = document.createElement("div");
+  popupError.classList.add("popupError");
+  popupError.innerHTML = `
+        <div
+          class="popupError__P"
+          style="background-image: url(./resources/svg/access_denied.svg)"
+        ></div>
+
+        <p class="popupError__topDesc"></p>
+
+        <div class="popupError__404">
+          <div class="popupError__4">
+            <div class="line-u" style="--timeL-u: 0.25s"></div>
+            <div class="line-u" style="--timeL-u: 0.5s"></div>
+            <div class="line-4"></div>
+          </div>
+          <div class="popupError__0">
+            <div class="line-u0 line-u0--1"></div>
+            <div class="line-u0 line-u0--2"></div>
+            <div class="line-u0 line-u0--3"></div>
+          </div>
+          <div class="popupError__4">
+            <div class="line-u" style="--timeL-u: 0.75s"></div>
+            <div class="line-u" style="--timeL-u: 1s"></div>
+            <div class="line-4"></div>
+          </div>
+        </div>
+
+        <p class="popupError__desc">
+          "${e}" bạn vừa click chưa được lặp trình😭
+          <br />
+          ~(>_<。)＼
+          <br />
+          Sorry but something went wrong or the "${e}" you've just clicked is not
+          yet developed 😭
+        </p>
+  `;
+  popup.appendChild(popupError);
+  console.log(popupError);
+  console.log("1");
+}
+
+//get --notDeveloped
+const notDeved = document.querySelectorAll(".--notDeveloped");
+
+for (var i of notDeved) {
+  i.addEventListener("click", function (e) {
+    if (popup) popupError(this.innerText);
+  });
+}
