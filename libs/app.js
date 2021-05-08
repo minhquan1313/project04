4d379c2b82ae950189af69f17dead16ecd3ebaba
minhquan1313 <Minhquan1313@gmail.com>
2021-05-08 17:42:03 +0700

    a

--- libs/app.js
+++ libs/app.js
@@ -1,7 +1,38 @@
 function urlNameToNormalName(e) {
-  console.log(decodeURI(e));
   return decodeURI(e);
 }
+
+var redirectParentWebpage = document.getElementById("logo");
+
+redirectParentWebpage.onclick = function () {
+  if (window.location.protocol.includes("http")) {
+    let tempUrl = window.location.href;
+
+    let tempUrlLength = tempUrl.indexOf("/", 7);
+    let tempUrlPLength = tempUrl
+      .toLowerCase()
+      .indexOf("/project", tempUrlLength);
+
+    // if (tempUrlPLength >= 0) {
+    //   tempUrlPLength++;
+    //   console.log(tempUrl.slice(0, tempUrl.indexOf("/", tempUrlPLength)));
+    // } else {
+    //   console.log(tempUrl.slice(0, tempUrlLength));
+    // }
+
+    if (tempUrlPLength >= 0) {
+      tempUrlPLength++;
+      window.location.replace(
+        tempUrl.slice(0, tempUrl.indexOf("/", tempUrlPLength))
+      );
+    } else {
+      window.location.replace(tempUrl.slice(0, tempUrlLength));
+    }
+  } else
+    console.log("Failed to redirect to home page because this is not http(s)");
+
+  // window.location.replace();
+};
 
 document.getElementById("bgAudio").volume = 0.1;
 
@@ -24,7 +55,6 @@ var bgMusic = document.getElementById("bgAudio");
 //Lay ten bai hat
 var musicName = [bgMusic.src].toString().split("/").pop();
 musicName = musicName.slice(0, musicName.lastIndexOf("."));
-// console.log(musicName);
 musicName = urlNameToNormalName(musicName);
 
 var musicPaused = document.querySelector('i[class*="ti-control-play"]');
@@ -45,26 +75,28 @@ document.onreadystatechange = () => {
       }
     }, 200);
 
-    audioControl.onclick = function () {
-      // bgMusic.duration > 0 &&
-      if (!isNaN(bgMusic.duration) && bgMusic.duration > 0 && !bgMusic.paused) {
-        musicPlaying.setAttribute("style", "display: none");
-        musicPaused.setAttribute("style", "display: block");
-        bgMusic.pause();
-      } else if (!isNaN(bgMusic.duration)) {
-        musicPlaying.setAttribute("style", "display: block");
-        musicPaused.setAttribute("style", "display: none");
-        bgMusic.play();
-      }
+    audioControl.onclick = function (e) {
+      if (e.target.closest("i[class*='ti-']"))
+        if (
+          !isNaN(bgMusic.duration) &&
+          bgMusic.duration > 0 &&
+          !bgMusic.paused
+        ) {
+          musicPlaying.setAttribute("style", "display: none");
+          musicPaused.setAttribute("style", "display: block");
+          bgMusic.pause();
+        } else if (!isNaN(bgMusic.duration)) {
+          musicPlaying.setAttribute("style", "display: block");
+          musicPaused.setAttribute("style", "display: none");
+          bgMusic.play();
+        }
     };
   }
 };
 
 const albumPics = document.querySelectorAll(".leftPanel__albumItem");
-// console.log(albumPics);
 for (var i of albumPics) {
   i.addEventListener("click", function (e) {
     let imgLink = e.target.src;
-    console.log(imgLink);
   });
 }
---
+++ project055/proJECt0009/libs/app.js
@@ -0,0 +1,94 @@
+function urlNameToNormalName(e) {
+  return decodeURI(e);
+}
+
+var redirectParentWebpage = document.getElementById("logo");
+
+redirectParentWebpage.onclick = function () {
+  let tempUrl = window.location.href;
+
+  let tempUrlLength = tempUrl.indexOf("/", 7);
+  let tempUrlPLength = tempUrl.toLowerCase().indexOf("/project", tempUrlLength);
+  if (tempUrlPLength >= 0) tempUrlPLength++;
+
+  console.log(tempUrlPLength);
+
+  // if (tempUrlPLength >= 0)
+  //   window.location.replace(
+  //     tempUrl.slice(0, tempUrl.indexOf("/", tempUrlPLength))
+  //   );
+  // else window.location.replace(tempUrl.slice(0, tempUrlLength));
+
+  if (tempUrlPLength >= 0)
+    console.log(tempUrl.slice(0, tempUrl.indexOf("/", tempUrlPLength)));
+  else console.log(tempUrl.slice(0, tempUrlLength));
+
+  // window.location.replace();
+};
+
+document.getElementById("bgAudio").volume = 0.1;
+
+//Thay doi khi re chuot cac Options
+var optionList = document.querySelectorAll("li.optionList__item");
+for (var i of optionList) {
+  i.addEventListener("mouseover", function () {
+    this.classList.add("optionList__item--selected");
+  });
+  i.addEventListener("mouseout", function () {
+    this.classList.remove("optionList__item--selected");
+  });
+}
+
+//Lay audio
+var audioControl = document.getElementById("audioControl");
+//Lay icon player cua audio
+var bgMusic = document.getElementById("bgAudio");
+
+//Lay ten bai hat
+var musicName = [bgMusic.src].toString().split("/").pop();
+musicName = musicName.slice(0, musicName.lastIndexOf("."));
+musicName = urlNameToNormalName(musicName);
+
+var musicPaused = document.querySelector('i[class*="ti-control-play"]');
+var musicPlaying = document.querySelector('i[class*="ti-control-pause"]');
+
+// <==> on("load",function(){})
+document.onreadystatechange = () => {
+  if (document.readyState === "complete") {
+    var nameAudio = audioControl.querySelector(".audioControl__Name");
+    nameAudio.innerHTML = musicName;
+
+    var d = setInterval(function () {
+      if (!bgMusic.paused) {
+        musicPlaying.setAttribute("style", "display: block");
+        musicPaused.setAttribute("style", "display: none");
+        bgMusic.play();
+        clearInterval(d);
+      }
+    }, 200);
+
+    audioControl.onclick = function (e) {
+      if (e.target.closest("i[class*='ti-']"))
+        if (
+          !isNaN(bgMusic.duration) &&
+          bgMusic.duration > 0 &&
+          !bgMusic.paused
+        ) {
+          musicPlaying.setAttribute("style", "display: none");
+          musicPaused.setAttribute("style", "display: block");
+          bgMusic.pause();
+        } else if (!isNaN(bgMusic.duration)) {
+          musicPlaying.setAttribute("style", "display: block");
+          musicPaused.setAttribute("style", "display: none");
+          bgMusic.play();
+        }
+    };
+  }
+};
+
+const albumPics = document.querySelectorAll(".leftPanel__albumItem");
+for (var i of albumPics) {
+  i.addEventListener("click", function (e) {
+    let imgLink = e.target.src;
+  });
+}
