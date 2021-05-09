0c555f50e653daab568db6d509e76068acb6ff74
minhquan1313 <Minhquan1313@gmail.com>
2021-05-09 22:06:29 +0700

    a

--- libs/app.js
+++ libs/app.js
@@ -104,6 +104,7 @@ for (var i of albumPics) {
 //PopUp
 const popup = document.getElementById("popup");
 const delay = 200;
+const bgVideo = document.getElementById("bgVideo");
 
 function getPopup() {
   popup.setAttribute(
@@ -114,71 +115,146 @@ function getPopup() {
     popup.style.opacity = 1;
   }, 0);
 }
-
 function shutPopup() {
-  popup.setAttribute("style", "display: none");
+  popup.setAttribute(
+    "style",
+    `transition: all ${delay / 1000}s ease; pointer-events: none; opacity: 0;`
+  );
+
+  setTimeout(function () {
+    popup.setAttribute("style", "display: none;");
+    popup.innerHTML = "";
+  }, delay);
 }
 
+function playBackgroundVideo(e) {
+  if (!e.play()) {
+    e.play();
+  }
+}
+function pauseBackgroundVideo(e) {
+  if (!e.pause()) {
+    e.pause();
+  }
+}
+// -----------------------------------------------------------------
 popup.onclick = function (e) {
   if (e.target.className == popup.className) {
-    // console.log("Clicked popup background only");
-
-    popup.setAttribute(
-      "style",
-      `transition: all ${delay / 1000}s ease; pointer-events: none; opacity: 0;`
-    );
-
-    setTimeout(function () {
-      popup.setAttribute("style", "display: none;");
-      popup.innerHTML = "";
-    }, delay);
+    playBackgroundVideo(bgVideo);
+    shutPopup();
   }
 };
-
 //Error
+// console.log(popup.tagName);
 function popupError(e) {
-  // console.log(e);
   getPopup();
   const popupError = document.createElement("div");
   popupError.classList.add("popupError");
+  let text = e.innerText;
+
+  if (text == "") {
+    text = e.tagName;
+    if (text != "BUTTON" && text != "IMG") {
+      let check = e.querySelectorAll("*");
+      if (check) {
+        let checkFor = false;
+        for (var i of check) {
+          if (i.tagName == "BUTTON" || i.tagName == "IMG") {
+            text = i.tagName;
+            if (text != "IMG") {
+              text = text.toLowerCase();
+              text = text.replace(text.charAt(0), text.charAt(0).toUpperCase());
+            } else text = "Image";
+            checkFor = true;
+            break;
+          }
+        }
+        if (!checkFor) text = "Unknown";
+      } else text = "Unknown";
+    } else {
+      text = text.toLowerCase();
+      text = text.replace(text.charAt(0), text.charAt(0).toUpperCase());
+    }
+  }
   popupError.innerHTML = `
-        <div
-          class="popupError__P"
-          style="background-image: url(./resources/svg/access_denied.svg)"
-        ></div>
+  <div class="popupError__P">
+  <div class="popupError__P-phone">
+    <div class="whiteScreen">
+      <div class="topBunnyEar"></div>
 
-        <p class="popupError__topDesc"></p>
+      <div class="roundShape">
+        <div class="xShape" style="--rotate: 45deg"></div>
 
-        <div class="popupError__404">
-          <div class="popupError__4">
-            <div class="line-u" style="--timeL-u: 0.25s"></div>
-            <div class="line-u" style="--timeL-u: 0.5s"></div>
-            <div class="line-4"></div>
-          </div>
-          <div class="popupError__0">
-            <div class="line-u0 line-u0--1"></div>
-            <div class="line-u0 line-u0--2"></div>
-            <div class="line-u0 line-u0--3"></div>
-          </div>
-          <div class="popupError__4">
-            <div class="line-u" style="--timeL-u: 0.75s"></div>
-            <div class="line-u" style="--timeL-u: 1s"></div>
-            <div class="line-4"></div>
+        <div class="xShape" style="--rotate: -45deg"></div>
+      </div>
+
+      <div class="msgBox">
+        <div class="msgBox__inner">
+          <div class="roundShape roundShape--msg">
+            <div class="xShape" style="--rotate: 0deg"></div>
           </div>
         </div>
 
-        <p class="popupError__desc">
-          "${e}" bạn vừa click chưa được lặp trình😭
-          <br />
-          ~(>_<。)＼
-          <br />
-          Sorry but something went wrong or the "${e}" you've just clicked is not
-          yet developed 😭
-        </p>
+        <div class="msgBox__inner">
+          <div class="roundShape roundShape--msg">
+            <div class="xShape" style="--rotate: 0deg"></div>
+          </div>
+        </div>
+
+        <div class="msgBox__inner">
+          <div class="roundShape roundShape--msg">
+            <div class="xShape" style="--rotate: 0deg"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="popupError__P-lineWFlower">
+    <div class="line">
+      <div class="flower1">
+        <div class="flower1__canh"></div>
+        <div class="flower1__hoa"></div>
+      </div>
+
+      <div class="flower2">
+        <div class="flower2__canh"></div>
+
+        <div class="flower2__hoa-sub flower2__hoa-subP1"></div>
+        <div class="flower2__hoa"></div>
+        <div class="flower2__hoa-sub flower2__hoa-subP2"></div>
+        <!-- ... -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="popupError__404">
+  <div class="popupError__404-inner">
+    <div class="popupError__4">
+      <div class="line-u" style="--timeL-u: 0.25s"></div>
+      <div class="line-u" style="--timeL-u: 1s"></div>
+      <div class="line-4"></div>
+    </div>
+    <div class="popupError__0">
+      <div class="line-u0 line-u0--1"></div>
+      <div class="line-u0 line-u0--2"></div>
+      <div class="line-u0 line-u0--3"></div>
+    </div>
+    <div class="popupError__4">
+      <div class="line-u" style="--timeL-u: 0.75s"></div>
+      <div class="line-u" style="--timeL-u: 0.5s"></div>
+      <div class="line-4"></div>
+    </div>
+  </div>
+</div>
+
+<p class="popupError__desc">
+  Sorry the ${text} is not yet developed 😭
+</p>
   `;
+  pauseBackgroundVideo(bgVideo);
   popup.appendChild(popupError);
-  console.log(popupError);
-  console.log("1");
 }
 
 //get --notDeveloped
@@ -186,6 +262,6 @@ const notDeved = document.querySelectorAll(".--notDeveloped");
 
 for (var i of notDeved) {
   i.addEventListener("click", function (e) {
-    if (popup) popupError(this.innerText);
+    if (popup) popupError(this);
   });
 }
