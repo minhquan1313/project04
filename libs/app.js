d73d6cb51cbc43f9d9fb0e1882f7f358bf5d10d8
minhquan1313 <Minhquan1313@gmail.com>
2021-05-11 12:40:07 +0700

    a

--- libs/app.js
+++ libs/app.js
@@ -1,9 +1,5 @@
-function urlNameToNormalName(e) {
-  return decodeURI(e);
-}
-
+// Getting url for embedding to the website logo --------------------
 var redirectParentWebpage = document.getElementById("logo");
-
 redirectParentWebpage.onclick = function () {
   if (window.location.protocol.includes("http")) {
     let tempUrl = window.location.href;
@@ -36,7 +32,7 @@ redirectParentWebpage.onclick = function () {
 
 document.getElementById("bgAudio").volume = 0.1;
 
-//Thay doi khi re chuot cac Options
+// Track mouse when hover over options on screen --------------------
 var optionList = document.querySelectorAll("li.optionList__item");
 for (var i of optionList) {
   i.addEventListener("mouseover", function () {
@@ -47,20 +43,27 @@ for (var i of optionList) {
   });
 }
 
-//Lay audio
+// Get audio Controller (play/pause) --------------------------------
 var audioControl = document.getElementById("audioControl");
-//Lay icon player cua audio
+// Get background audio element
 var bgMusic = document.getElementById("bgAudio");
 
-//Lay ten bai hat
+// Decode URL text to normal text that people can read --------------
+function urlNameToNormalName(e) {
+  return decodeURI(e);
+}
+// Get audio's filename
 var musicName = [bgMusic.src].toString().split("/").pop();
 musicName = musicName.slice(0, musicName.lastIndexOf("."));
 musicName = urlNameToNormalName(musicName);
 
+// Get the play and pause elements in DOM ---------------------------
 var musicPaused = document.querySelector('i[class*="ti-control-play"]');
 var musicPlaying = document.querySelector('i[class*="ti-control-pause"]');
 
 // <==> on("load",function(){})
+// Wait until DOM is fully loaded then start checking status of
+// background music and to change play/pause button when clicked
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
     var nameAudio = audioControl.querySelector(".audioControl__Name");
@@ -94,18 +97,20 @@ document.onreadystatechange = () => {
   }
 };
 
-const albumPics = document.querySelectorAll(".leftPanel__albumItem");
-for (var i of albumPics) {
-  i.addEventListener("click", function (e) {
-    let imgLink = e.target.src;
-  });
-}
+// Left hand pictures -----------------------------------------------
+// const albumPics = document.querySelectorAll(".leftPanel__albumItem");
+// for (var i of albumPics) {
+//   i.addEventListener("click", function (e) {
+//     let imgLink = e.target.src;
+//   });
+// }
 
-//PopUp
+//PopUp initializing ------------------------------------------------
 const popup = document.getElementById("popup");
 const delay = 200;
 const bgVideo = document.getElementById("bgVideo");
 
+// Initialize some function for PopUp -------------------------------
 function getPopup() {
   popup.setAttribute(
     "style",
@@ -115,12 +120,12 @@ function getPopup() {
     popup.style.opacity = 1;
   }, 0);
 }
+
 function shutPopup() {
   popup.setAttribute(
     "style",
     `transition: all ${delay / 1000}s ease; pointer-events: none; opacity: 0;`
   );
-
   setTimeout(function () {
     popup.setAttribute("style", "display: none;");
     popup.innerHTML = "";
@@ -132,26 +137,28 @@ function playBackgroundVideo(e) {
     e.play();
   }
 }
+
 function pauseBackgroundVideo(e) {
   if (!e.pause()) {
     e.pause();
   }
 }
-// -----------------------------------------------------------------
+// When click the popup background ----------------------------------
 popup.onclick = function (e) {
   if (e.target.className == popup.className) {
     playBackgroundVideo(bgVideo);
     shutPopup();
   }
 };
-//Error
+//Error -------------------------------------------------------------
 // console.log(popup.tagName);
 function popupError(e) {
   getPopup();
   const popupError = document.createElement("div");
   popupError.classList.add("popupError");
-  let text = e.innerText;
 
+  // Get text in the clicked element --------------------------------
+  let text = e.innerText;
   if (text == "") {
     text = e.tagName;
     if (text != "BUTTON" && text != "IMG") {
@@ -176,92 +183,112 @@ function popupError(e) {
       text = text.replace(text.charAt(0), text.charAt(0).toUpperCase());
     }
   }
+
+  // Set content for the popupError variable ------------------------
   popupError.innerHTML = `
-  <div class="popupError__P">
-  <div class="popupError__P-phone">
-    <div class="whiteScreen">
-      <div class="topBunnyEar"></div>
+      <div class="popupError">
+        <div class="popupError__P">
+          <div class="popupError__P-phone">
+            <div class="whiteScreen">
+              <div class="topBunnyEar"></div>
 
-      <div class="roundShape">
-        <div class="xShape" style="--rotate: 45deg"></div>
+              <div class="roundShape">
+                <div
+                  class="xShape"
+                  style="--rotate: 45deg; --xShape-delay: 0.2s"
+                ></div>
 
-        <div class="xShape" style="--rotate: -45deg"></div>
-      </div>
+                <div
+                  class="xShape"
+                  style="--rotate: -45deg; --xShape-delay: 0s"
+                ></div>
+              </div>
 
-      <div class="msgBox">
-        <div class="msgBox__inner">
-          <div class="roundShape roundShape--msg">
-            <div class="xShape" style="--rotate: 0deg"></div>
+              <div class="msgBox">
+                <div class="msgBox__inner" style="--xShape-delay: 0.4s">
+                  <div class="roundShape roundShape--msg">
+                    <div
+                      class="xShape"
+                      style="--rotate: 0deg; --xShape-delay: 0.6s"
+                    ></div>
+                  </div>
+                </div>
+
+                <div class="msgBox__inner" style="--xShape-delay: 0.5s">
+                  <div class="roundShape roundShape--msg">
+                    <div
+                      class="xShape"
+                      style="--rotate: 0deg; --xShape-delay: 0.7s"
+                    ></div>
+                  </div>
+                </div>
+
+                <div class="msgBox__inner" style="--xShape-delay: 0.6s">
+                  <div class="roundShape roundShape--msg">
+                    <div
+                      class="xShape"
+                      style="--rotate: 0deg; --xShape-delay: 0.8s"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="popupError__P-lineWFlower">
+            <div class="line">
+              <div class="flower1">
+                <div class="flower1__canh"></div>
+                <div class="flower1__hoa"></div>
+              </div>
+
+              <div class="flower2">
+                <div class="flower2__canh"></div>
+
+                <div class="flower2__hoa-sub flower2__hoa-subP1"></div>
+                <div class="flower2__hoa"></div>
+                <div class="flower2__hoa-sub flower2__hoa-subP2"></div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div class="msgBox__inner">
-          <div class="roundShape roundShape--msg">
-            <div class="xShape" style="--rotate: 0deg"></div>
+        <div class="popupError__404">
+          <div class="popupError__404-inner">
+            <div class="popupError__4">
+              <div class="line-u" style="--timeL-u: 0s"></div>
+              <div class="line-u" style="--timeL-u: 0.3s"></div>
+              <div class="line-4"></div>
+            </div>
+            <div class="popupError__0">
+              <div class="line-u0 line-u0--1"></div>
+              <div class="line-u0 line-u0--2"></div>
+              <div class="line-u0 line-u0--3"></div>
+            </div>
+            <div class="popupError__4">
+              <div class="line-u" style="--timeL-u: 0.1s"></div>
+              <div class="line-u" style="--timeL-u: 0.4s"></div>
+              <div class="line-4"></div>
+            </div>
           </div>
         </div>
 
-        <div class="msgBox__inner">
-          <div class="roundShape roundShape--msg">
-            <div class="xShape" style="--rotate: 0deg"></div>
-          </div>
-        </div>
+        <p class="popupError__desc">
+          Sorry the ${text} is not yet developed 😭
+        </p>
       </div>
-    </div>
-  </div>
-
-  <div class="popupError__P-lineWFlower">
-    <div class="line">
-      <div class="flower1">
-        <div class="flower1__canh"></div>
-        <div class="flower1__hoa"></div>
-      </div>
-
-      <div class="flower2">
-        <div class="flower2__canh"></div>
-
-        <div class="flower2__hoa-sub flower2__hoa-subP1"></div>
-        <div class="flower2__hoa"></div>
-        <div class="flower2__hoa-sub flower2__hoa-subP2"></div>
-        <!-- ... -->
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="popupError__404">
-  <div class="popupError__404-inner">
-    <div class="popupError__4">
-      <div class="line-u" style="--timeL-u: 0.25s"></div>
-      <div class="line-u" style="--timeL-u: 1s"></div>
-      <div class="line-4"></div>
-    </div>
-    <div class="popupError__0">
-      <div class="line-u0 line-u0--1"></div>
-      <div class="line-u0 line-u0--2"></div>
-      <div class="line-u0 line-u0--3"></div>
-    </div>
-    <div class="popupError__4">
-      <div class="line-u" style="--timeL-u: 0.75s"></div>
-      <div class="line-u" style="--timeL-u: 0.5s"></div>
-      <div class="line-4"></div>
-    </div>
-  </div>
-</div>
-
-<p class="popupError__desc">
-  Sorry the ${text} is not yet developed 😭
-</p>
   `;
+
+  // Pause the background video and then write the popupError
+  // variable to DOM
   pauseBackgroundVideo(bgVideo);
   popup.appendChild(popupError);
 }
 
-//get --notDeveloped
+//get --notDeveloped classes in DOM ---------------------------------
 const notDeved = document.querySelectorAll(".--notDeveloped");
-
 for (var i of notDeved) {
   i.addEventListener("click", function (e) {
-    if (popup) popupError(this);
+    if (popup && popup.style.display == "none") popupError(this);
   });
 }
